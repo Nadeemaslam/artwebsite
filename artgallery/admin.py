a7129fad9268df0ecbeefeeb90ccb6d55c6e3ba6
Nomaan <nomaanahmad77@gmail.com>
2021-11-03 14:04:11 +0530

    made changes to structure

--- artgallery/admin.py
+++ artgallery/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Painting, Categories
 
 # Register your models here.
+
+
+class AdminPainting(admin.ModelAdmin):
+    list_display = ['title', 'year', 'category']
+    prepopulated_fields = {"slug": ("title",)}
+
+
+admin.site.register(Painting, AdminPainting)
+admin.site.register(Categories)
